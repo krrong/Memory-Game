0e74b3f220fb56c0eadbb808895a87d7b0f682b8
krrong <khk9664@naver.com>
2022-05-07 01:17:17 +0900

    게임화면 수정
    시작 버튼을 클릭하고나면 화면에 숫자 버튼이 나오도록 수정

--- memory game.py
+++ memory game.py
@@ -1,4 +1,49 @@
 import pygame
+from random import *
+
+# 레벨에 맞게 설정
+def setup(level):
+    # 얼마나 많은 숫자를 보여줄 것인가?
+    number_cnt = (level // 3) + 5
+    number_cnt = min(number_cnt, 20)
+    
+    # 실제 화면에 grid 형태로 숫자를 랜덤으로 배치
+    shuffle_grid(number_cnt)
+    
+# 숫자 섞기
+def shuffle_grid(number_cnt):
+    rows = 5
+    columns = 9
+    
+    cell_size = 130             # 각 grid cell의 가로, 세로 크기
+    button_size = 110           # grid cell내에 실제로 그려질 버튼의 크기
+    screen_left_margin = 50     # 전체 스크린 왼쪽 여백
+    screen_top_margin = 20      # 전체 스크린 위쪽 여백
+    
+    # 5 x 9 grid(list) 생성
+    grid = [[0 for column in range(columns)] for row in range(rows)]
+    
+    # 시작 숫자
+    number = 1
+    while number <= number_cnt:
+        row_index = randrange(0, rows) # 0 <= row_index < rows (0~4)
+        col_index = randrange(0, columns) # 0 <= col_index < columns (0~8)
+        
+        # 빈 값이면 값을 넣어주고 number 증가
+        if grid[row_index][col_index] == 0:
+            grid[row_index][col_index] = number
+            number = number + 1
+            
+            # 현재 grid cell 위치 기준으로 x, y 위치를 구함
+            center_x = screen_left_margin + (col_index * cell_size) + cell_size // 2
+            center_y = screen_top_margin + (row_index * cell_size) + cell_size // 2
+            
+            # 숫자 버튼 만들기
+            button = pygame.Rect(0, 0, button_size, button_size)
+            button.center = (center_x, center_y)
+            
+            number_buttons.append(button)
+
 
 # 시작 화면 보여주는 함수
 def display_start_screen():
@@ -8,7 +53,15 @@ def display_start_screen():
 
 # 게임 화면 보여주는 함수
 def display_game_screen():
-    print("Game Start")
+    for idx, rect in enumerate(number_buttons, start=1):
+        pygame.draw.rect(screen, GRAY, rect)
+        
+        # 실제 숫자 텍스트
+        cell_text = game_font.render(str(idx), True, WHITE)
+        text_rect = cell_text.get_rect(center = rect.center)
+        screen.blit(cell_text, text_rect)
+        
+        
 
 # position에 해당하는 버튼 확인
 def check_buttons(position):
@@ -25,6 +78,7 @@ screen_width = 1280 # 가로 크기
 screen_height = 720 # 가로 크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Memory Game")
+game_font = pygame.font.Font(None, 120) # 폰트 정의
 
 # 시작 버튼
 start_button = pygame.Rect(0, 0, 120, 120)          # 사각형의 크기가 120x120
@@ -33,9 +87,16 @@ start_button.center = (120, screen_height - 120)    # 사각형 중심좌표 설
 # 색상
 BLACK = (0, 0, 0) # RGC
 WHITE = (255, 255, 255)
+GRAY = (50, 50, 50)
+
+# 플레이어가 눌러야 하는 버튼을 관리하는 리스트
+number_buttons = [] 
 
 # 게임 시작 여부 
 start = False
+
+# 게임 시작 전에 게임 설정 함수 수행
+setup(1)
 
 # 게임 루프
 running = True  # 게임이 실행중인지 체크
